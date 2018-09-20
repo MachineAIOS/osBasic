268e633182ee2fd37415c485e71c4f8ff662b2ca
robot000000001 <38873402+robot000000001@users.noreply.github.com>
2018-09-20 20:51:42 +0800

    a basic AI code of robot
    A basic AI code of robot,show build a ai basic way;

--- main.cpp
+++ main.cpp
@@ -1,8 +1,39 @@
 #include "stdio.h"
 #include "stdlib.h"
 #include "string.h"
+Inforamtion hugePool1;
+cell hugePool2;
+class cell
+{
+   int stop=0;
+   int init()
+   {
+     stop=0;
+     createThread(loopThread);
+   }
+   void loopThread()
+   {
+      initRobot();
+      while(!stop)
+      {
+         inputInforamtion();//get information from pool
+         work();
+         outputInforamtion();//write information to pool
+      }
+   }
+}
 
 void main(int argc,char **argv)
 {
   printf("a ai os system which it change world!\n");
+  while(1)
+  {
+    input_image();
+    input_audio();
+    build_net();
+    learn_net();
+    cell_built();
+    act();
+    clear();
+  }
 }
